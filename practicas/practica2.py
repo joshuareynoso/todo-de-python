a1cfb74d2c7d9d8da884050bee6318212fe8a7cd
joshuareynoso <134154349+joshuareynoso@users.noreply.github.com>
2024-07-24 14:56:46 -0300

    apa la papa
    pipi

--- practicas/practica2.py
+++ practicas/practica2.py
@@ -1,3 +1,9 @@
-
-
- 
+#problema de caja 
+"""
+Imagina que estás diseñando un sistema para una tienda de almacenamiento. 
+Necesitas crear una clase Caja que permita almacenar y gestionar elementos.
+La clase debe tener las siguientes funcionalidades:
+"""
+#inicio del programa 
+print("")
+caja_vacia= []
